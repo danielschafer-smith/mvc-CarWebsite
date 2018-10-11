e76cce76a46db36ea5aa6966f94dd86eb97c8527
Daniel Schafer-Smith <21701763@bucks.ac.uk>
2018-10-11 10:24:53 +0100

    Add project files.

---
+++ mvcCarWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcCarWebsite.Startup))]
+namespace mvcCarWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
